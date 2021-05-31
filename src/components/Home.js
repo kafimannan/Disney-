13b9fa6dbe4510a060201daca8f068ebafff25e7
kafi mannan <mannan.kafi@gmail.com>
2021-05-31 18:39:37 +0600

    Recomended for you

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
+import Movies from './Movies';
 
 
 
@@ -11,6 +12,7 @@ function Home()
         <Container>
             <ImgSlider />
             <Viewers />
+            <Movies />
         </Container>
     )
 }
